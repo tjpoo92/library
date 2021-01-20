a91ea0f6d8fefe6d1624aaeb0ee7d6bf2f89176a
Tyler Pooschke <tjpoo92@gmail.com>
2021-01-19 23:22:45 -0600

    basic GUI completed, starting on logic

---
+++ index.js
@@ -0,0 +1,12 @@
+//Array
+let myLibrary = [];
+
+//Constructor
+function Book() {}
+
+//Add Book Function
+function addBookToLibrary() {}
+
+function openForm() {
+  document.getElementById("bookForm").classList.toggle("display");
+}
