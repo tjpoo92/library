39093f592edb2007a6ccee775946d6429d0eba03
Tyler Pooschke <tjpoo92@gmail.com>
2021-01-23 15:28:16 -0600

    finished basic form and contructor

--- index.js
+++ index.js
@@ -2,11 +2,47 @@
 let myLibrary = [];
 
 //Constructor
-function Book() {}
+function Book(title, author, numberOfPages, readStatus) {
+  this.bookTitle = title;
+  this.bookAuthor = author;
+  this.bookNumberOfPages = numberOfPages;
+  this.bookReadStatus = readStatus;
+  this.bookArrayPosition = NaN;
+}
 
 //Add Book Function
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  myLibrary.push(newBook);
+  clearForm();
+  newBook.bookArrayPosition = myLibrary.indexOf(newBook);
+  console.table(myLibrary);
+}
 
-function openForm() {
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+  collectFormInfo();
+  event.preventDefault();
+});
+
+function toggleForm() {
   document.getElementById("bookForm").classList.toggle("display");
+}
+
+let newBook;
+function collectFormInfo() {
+  const formAuthor = document.querySelector("#author").value;
+  const formTitle = document.querySelector("#title").value;
+  const formNumberOfPages = document.querySelector("#numberOfPages").value;
+  const formReadStatus = document.querySelector("#read").checked;
+  newBook = new Book(
+    `${formTitle}`,
+    `${formAuthor}`,
+    `${formNumberOfPages}`,
+    `${formReadStatus}`
+  );
+  addBookToLibrary();
+}
+
+function clearForm() {
+  form.reset();
 }
