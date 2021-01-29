912266db0e0f67f9d0f7d9a738c7182d3ad024d2
Tyler Pooschke <tjpoo92@gmail.com>
2021-01-28 22:59:39 -0600

    created the basic logic to delete books, added duplicate checking when adding books in form, add comments to clear up code

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 //Array
-let myLibrary = [];
+let myLibrary = [
+  {
+    bookTitle: "Where the Wild Things Are",
+    bookAuthor: "MS",
+    bookNumberOfPages: 15,
+    bookReadStatus: false,
+    bookArrayPosition: 0,
+  },
+];
 
 //Constructor
 function Book(title, author, numberOfPages, readStatus) {
@@ -10,64 +18,85 @@ function Book(title, author, numberOfPages, readStatus) {
   this.bookArrayPosition = NaN;
 }
 
+//TODO: Duplicate checking on book creation, need to refactor into own function
+
 //Add Book Function
 function addBookToLibrary() {
-  myLibrary.push(newBook);
-  clearForm();
-  newBook.bookArrayPosition = myLibrary.indexOf(newBook);
-  console.table(myLibrary);
-  createBookCards();
-}
+  const allTitles = document.querySelectorAll("h4");
 
-function createBookCards() {
-  for (let i = 0; i < myLibrary.length; i++) {
-    h4.textContent = myLibrary[i].bookTitle;
-    paraAuthor.textContent = myLibrary[i].bookAuthor;
-    paraPages.textContent = myLibrary[i].bookNumberOfPages;
+  //Error check for matching entries
+  for (let i = 0; i < allTitles.length; i++) {
+    if (
+      //Catch regardless of casing
+      allTitles[i].textContent.toLowerCase() == newBook.bookTitle.toLowerCase()
+    ) {
+      window.alert("This entry has already been added");
+      clearForm();
+      continue;
+    } else {
+      myLibrary.push(newBook);
+      clearForm();
+      newBook.bookArrayPosition = myLibrary.indexOf(newBook);
+      console.table(myLibrary);
+      createBookCards();
+    }
   }
-  document
-    .querySelector(".body")
-    .appendChild(divCard)
-    .appendChild(button)
-    .insertAdjacentElement("afterend", divCardContainer)
-    .appendChild(h4)
-    .insertAdjacentElement("afterend", br)
-    .insertAdjacentElement("afterend", paraAuthor)
-    .insertAdjacentElement("afterend", paraPages)
-    .insertAdjacentElement("afterend", formCard)
-    .appendChild(label);
+}
+const bodyContainer = document.querySelector(".body");
+function createBookCards() {
+  myLibrary.forEach((element) => {
+    //Create the nodes
+    const divCard = document.createElement("div");
+    const divCardContainer = document.createElement("div");
+    const button = document.createElement("button");
+    const h4 = document.createElement("h4");
+    const br = document.createElement("br");
+    const paraAuthor = document.createElement("p");
+    const paraPages = document.createElement("p");
+    const formCard = document.createElement("form");
+    const label = document.createElement("label");
+
+    //Give the nodes text from the form
+    h4.textContent = element.bookTitle;
+    paraAuthor.textContent = element.bookAuthor;
+    paraPages.textContent = `${element.bookNumberOfPages} pages`;
+
+    //Give classes to all nodes
+    divCard.classList.add("card");
+    button.classList.add("delete");
+    button.setAttribute("value", element.bookArrayPosition);
+    button.textContent = "X";
+    divCardContainer.classList.add("card-container");
+    h4.classList.add("title");
+    paraAuthor.classList.add("author");
+    paraPages.classList.add("pages");
+    formCard.setAttribute("action", "#");
+    //Issues with simply adding the node and text content, innerHTML resolved the issue
+    label.innerHTML = '<input type="checkbox" name="read" value="Read" />Read';
+
+    //Build out the book card
+    bodyContainer
+      .appendChild(divCard)
+      .appendChild(button)
+      .insertAdjacentElement("afterend", divCardContainer)
+      .appendChild(h4)
+      .insertAdjacentElement("afterend", br)
+      .insertAdjacentElement("afterend", paraAuthor)
+      .insertAdjacentElement("afterend", paraPages)
+      .insertAdjacentElement("afterend", formCard)
+      .appendChild(label);
+  });
+  addDeleteListener();
 }
 
-//Book Cards
-const divCard = document.createElement("div");
-const divCardContainer = document.createElement("div");
-const button = document.createElement("button");
-const h4 = document.createElement("h4");
-const br = document.createElement("br");
-const paraAuthor = document.createElement("p");
-const paraPages = document.createElement("p");
-const formCard = document.createElement("form");
-const label = document.createElement("label");
-
-divCard.classList.add("card");
-button.classList.add("delete");
-button.textContent = "X";
-divCardContainer.classList.add("card-container");
-h4.classList.add("title");
-h4.textContent = "Where the Wild Things Are";
-paraAuthor.classList.add("author");
-paraAuthor.textContent = "Maurice Sendak";
-paraPages.classList.add("pages");
-paraPages.textContent = "15 pages";
-formCard.setAttribute("action", "#");
-label.innerHTML = '<input type="checkbox" name="read" value="Read" />Read';
-
+//Stop form default behavior and send to function
 const form = document.querySelector("form");
 form.addEventListener("submit", (event) => {
   collectFormInfo();
   event.preventDefault();
 });
 
+//Show/hide add book form
 function toggleForm() {
   document.getElementById("bookForm").classList.toggle("display");
 }
@@ -85,6 +114,24 @@ function collectFormInfo() {
     `${formReadStatus}`
   );
   addBookToLibrary();
+}
+
+function addDeleteListener() {
+  const deleteButtons = document.querySelectorAll(".delete");
+  for (let i = 0; i < deleteButtons.length; i++) {
+    deleteButtons[i].addEventListener("click", (e) => {
+      myLibrary.pop(e.target.value);
+      clearBooks();
+      createBookCards();
+      console.table(myLibrary);
+    });
+  }
+}
+
+function clearBooks() {
+  while (bodyContainer.hasChildNodes()) {
+    bodyContainer.removeChild(bodyContainer.firstChild);
+  }
 }
 
 function clearForm() {
